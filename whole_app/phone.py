9c4eaca0e7ff45d0c3d16c61b48dff673df24539
luke <lukasz.ronka@gmail.com>
2019-12-21 11:39:28 +0100

    Tabs -> Spaces

--- whole_app/phone.py
+++ whole_app/phone.py
@@ -8,19 +8,20 @@ import logging.handlers
 
 
 def config_logging():
-	logging.basicConfig(filename="phone.log", style='{', format="{asctime} : {levelname:8} : {name:11} : {message}"
-						, level=logging.DEBUG)
-	return logging.getLogger(__name__)
+    logging.basicConfig(filename="phone.log", style='{', format="{asctime} : {levelname:8} : {name:11} : {message}"
+                        , level=logging.DEBUG)
+    return logging.getLogger(__name__)
+
 
 if __name__ == '__main__':
-	logger = config_logging()
-	logger.info("Application started")
-	queue = queue.Queue()
-	rotary_dial_thread = threading.Thread(target=rotary_dial.collect_number, args=(queue,))
-	rotary_dial_thread.start()
-	logger.info("Rotary dial thread started")
-	while True:
-		msg = queue.get()
-		logger.info(f"Message received: {msg}")
-		print(f"Received number to call: {msg}")
+    logger = config_logging()
+    logger.info("Application started")
+    queue = queue.Queue()
+    rotary_dial_thread = threading.Thread(target=rotary_dial.collect_number, args=(queue,))
+    rotary_dial_thread.start()
+    logger.info("Rotary dial thread started")
+    while True:
+        msg = queue.get()
+        logger.info(f"Message received: {msg}")
+        print(f"Received number to call: {msg}")
 
