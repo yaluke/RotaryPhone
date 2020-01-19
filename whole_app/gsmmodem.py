9445cc3d31e7e907d7324c71125125fd193f28d9
luke <lukasz.ronka@gmail.com>
2020-01-19 21:16:20 +0100

    Receiving and decoding sms. Forwarding to e-mail still to be done

--- whole_app/gsmmodem.py
+++ whole_app/gsmmodem.py
@@ -4,6 +4,8 @@ import time
 import queue
 import threading
 import os
+#import smsutil
+import smsforwarder
 
 
 class GsmModem:
@@ -33,13 +35,27 @@ class GsmModem:
 
         self.__execute_command('AT+CRSL=8', 'setting ring sound level', ['OK', '+CME ERROR'])
 
+        # 0 = PDU (default) 1 = text (mmses don't work in text mode)
+        self.__execute_command('AT+CMGF=0', 'Select SMS message format (PDU mode)', ['OK'])
+
+        # <mt> = 2 SMS-DELIVERs are routed directly to the TE using unsolicited result code: +CMT
+        self.__execute_command('AT+CNMI=0,2,0,0,0', 'New SMS message indications', ['OK'])
+
         self.__serial_data_thread = threading.Thread(target=self.__process_serial_data)
         self.__serial_data_thread.start()
 
         self.__ring = 0
+        self.__sms = 0
 
         self.__ring_thread = threading.Thread(target=self.__ring_loop)
         self.__ring_thread.start()
+
+#    def __process_sms(self, sms):
+#        print('Processing SMS')
+#        sms = b'\x41\xE1\x90\x08'
+#        t = smsutil.decode(sms, encoding='utf_8')
+#        print(t)
+#        print('End of processing SMS')
 
     def __process_serial_data(self):
         while True:
@@ -49,13 +65,26 @@ class GsmModem:
                     ret = str(ret, encoding='utf-8').rstrip('\r\n')
                 except UnicodeDecodeError as ude:
                     print(ude)
-                print(ret) if ret else None
+                self.logger.info(f'Received data from GSM modem:  {ret}') if ret else None
                 if ret.startswith('RING'):
                     self.__ring = 1
                 elif ret.startswith('ERROR'):
                     self.__ring = 0
                 elif ret.startswith('CONNECT'):
                     self.__ring = 0
+                elif ret.startswith('+CMT:'):
+                    # next line is a message
+                    ret = self.serial.readline()
+                    try:
+                        ret = str(ret, encoding='utf-8').rstrip('\r\n')
+                    except UnicodeDecodeError as ude:
+                        print(ude)
+                    else:
+                        self.logger.info(f'Received data from GSM modem (SMS):  {ret}')
+                        # new thread is created for every sms
+                        sms_forwarder_thread = threading.Thread(target=smsforwarder.forward_sms, args=(ret,))
+                        sms_forwarder_thread.start()
+
             time.sleep(0.1)
 
     def __ring_loop(self):
---
+++ whole_app/smsforwarder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Module responsible for decoding sms in PDU format and forwarding it to e-mail address
+"""
+
+import logging
+import smspdu.easy
+
+def forward_sms(sms):
+    logger = logging.getLogger(__name__)
+    logger.info('Decoding and forwarding SMS')
+    res = smspdu.easy.easy_sms(sms)
+    logger.info(f"SMS details: sender: {res['sender']}, date: {res['date']}, content: {res['content']}"
+                f", partial: {res['partial']}")
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="smsforwarder.log", style='{',
+                        format="{asctime} : {levelname:8} : {name:11} : {message}"
+                        , level=logging.DEBUG)
+    # sms1 = 'ABCD'
+    sms1 = ('07918406026030F1040B911032546576F00000021071918322400441E19008')
+    sms2 = ('07918406026030F14006D0CDE61400F502107191142340820B05040B8423F000031A02010006291F226170706C69636174696F6E2F'
+            '766E642E7761702E6D6D732D6D657373616765008184AF848D01008C82983174696434383630383339343935335F30377967673834'
+            '627068008D918918802B34383537323239323733362F545950453D504C4D4E00966E6F207375626A656374008A808E03')
+    sms3 = ('07918406026030F14406D0CDE61400F5021071911423405D0B05040B8423F000031A020203067188048102A8BE83687474703A2F2F'
+            '31302E37342E3138332E35352F6D6D732F776170656E633F6C6F636174696F6E3D34383630383339343935335F3037796767383462'
+            '7068267269643D31333700')
+    # sms2 & sms3 are two parts of mms (image)
+    forward_sms(sms1)
+    forward_sms(sms2)
+    forward_sms(sms3)
